8c2fa552d6d520e2f3874bbed39cfc07925df2fb
Enoch Nyako <enochnyako@gmail.com>
2018-10-19 09:26:58 +0000

    Update bloomer images with compressed version

--- sticker-keyboard/src/main/java/keyboard/android/psyphertxt/com/App.java
+++ sticker-keyboard/src/main/java/keyboard/android/psyphertxt/com/App.java
@@ -5,6 +5,7 @@ import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.answers.Answers;
+//import com.squareup.leakcanary.LeakCanary;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -17,6 +18,13 @@ public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            // This process is dedicated to LeakCanary for heap analysis.
+//            // You should not init your app in this process.
+//            return;
+//        }
+//        LeakCanary.install(this);
+        // Normal app init code...
         Fabric.with(App.this, new Answers(), new Crashlytics());
     }
 }
